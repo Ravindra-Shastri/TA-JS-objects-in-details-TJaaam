f8598221e4f71cc7d2ff7c3dadd0cc854945169c
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-09-14 16:22:29 +0530

    TODO.md

--- block-BJaahw/code/index.js
+++ block-BJaahw/code/index.js
@@ -1,19 +1,74 @@
 // 1. Create a function `createUser` that accepts `name` and `age` and returns a new object with those properties.
 
+function createUser(name,age){
+    return obj = {
+
+    }
+}
+
 // 2. Add a method named `sayHello` inside the object created above. When called it should alert `Welcome {user}`. Replace `{user}` with the name of the user.
+
+
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
 
 // 3. Use the data (name, age) of two different person to create the object using `createUser`. Store the returned value in `personOne` and `personTwo`.
 
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
+
 // 4. Change the code inside `createUser` in such a way that the methods `sayHello` doesn't have to be in all object. Use the prototypal nature. (Hint Object.create())
+
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
 
 // 5. Convert the `createUser` function into Pseudoclassical pattern of object creation. Use `F.prototype` to store the methods. `F.prototype` means storing the methods in prototype property of the function.
 
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
+
 // 6. Use `new` to create two new objects with the data of two different person and re-assign the value of `personOne` and `personTwo`.
+
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
 
 // 7. Try calling `personOne.sayHello()` and `personTwo.sayHello()`. Check if you get the required output.
 
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
+
 // 8. Convert the `createUser` function into `User` class.
 
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
+
 // 9. Check by creating two instance of the class using data of two different persons and re-assign the value of `personOne` and `personTwo`
+
+function createUser(name,age){
+    return obj = {
+        
+    }
+}
 
 // 10. Try calling `personOne.sayHello()` and `personTwo.sayHello()`. Check if you get the required output.
