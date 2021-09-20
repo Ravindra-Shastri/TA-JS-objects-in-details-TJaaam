d10653f0317339b20cd45e0aba3e2f29d7453052
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-09-20 14:42:21 +0530

    TODO.md

---
+++ block-BJaaik/code/index.js
@@ -0,0 +1,14 @@
+// Convert the give image into class using inheritance.
+
+// - `name` is the property
+// - `eat()` is the method
+
+// Property ending with `()` is method. And others are properties.
+
+// ![Inheritance](../assets/inheritance.png)
+
+class person{
+    constructor(){
+        this.name = name;
+    }
+}
